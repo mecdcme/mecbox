6bed4baacd23e720406c672d7f14ae89b7d2e244
andrea.pagano <andrea.pagano@pc75575.pc.istat.it>
2017-01-31 14:01:50 +0100

    prova commit : aggiunto commento

--- src/main/java/it/istat/mec/mecbox/MecboxApplication.java
+++ src/main/java/it/istat/mec/mecbox/MecboxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MecboxApplication {
 
+        // Add Comment Pag
 	public static void main(String[] args) {
 		SpringApplication.run(MecboxApplication.class, args);
 	}

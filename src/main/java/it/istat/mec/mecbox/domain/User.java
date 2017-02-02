ba753c96bf64413803e547d520812e93dbfc8e59
StefanoM <s.macone@libero.it>
2017-02-02 09:51:04 +0100

    prima commit2

--- src/main/java/it/istat/mec/mecbox/domain/User.java
+++ src/main/java/it/istat/mec/mecbox/domain/User.java
@@ -38,8 +38,11 @@ public class User implements Serializable {
      private String email;
     
  
- @Column(name = "fullname", length = 100)
- private String fullname;
+ @Column(name = "nome", length = 100)
+ private String nome;
+
+ @Column(name = "cognome", length = 100)
+ private String cognome;
 
  
 
@@ -56,15 +59,22 @@ private UserRole role;
 
 
 
-public String getFullname() {
-	return fullname;
+public String getNome() {
+	return nome;
 }
 
-public void setFullname(String fullname) {
-	this.fullname = fullname;
+public void setNome(String nome ) {
+	this.nome = nome;
 }
 
 
+public String getCognome() {
+	return cognome;
+}
+
+public void setCognome(String cognome) {
+	this.cognome = cognome;
+}
 
 
  

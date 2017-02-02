0756d446e5aae8afe2ca57c93fb3c3b78300026b
franco <mecbox.dcme@gmail.com>
2017-02-02 16:36:08 +0100

    o

---
+++ src/main/java/it/istat/mec/mecbox/dao/ReligionDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.istat.mec.mecbox.dao;
+
+import it.istat.mec.mecbox.domain.Religion;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author papizzo
+ */
+
+@Repository
+public interface ReligionDao extends CrudRepository<Religion,Long> {
+
+}

3bda954364c83d0c7222a3c7f1ff162c23bd02dc
papizzo <papizzo@PC69787.pc.istat.it>
2017-02-17 13:47:52 +0100

    household.js da html

---
+++ src/main/resources/public/js/household.js
@@ -0,0 +1,15 @@
+var _ctx = $("meta[name='ctx']").attr("content");
+$(document).ready(function () {
+    setMenuActive("household-list");
+    var table = $("#householdlist").DataTable({
+        responsive: true,
+        lengthChange: false,
+        pageLength: 15,
+        buttons: ['csv', 'excel', 'pdf'],
+        columnDefs: [
+            {targets: [1, 2, 3, 4, 5, 6, 7, 8, 9], render: $.fn.dataTable.render.number(',', '.', 0), 'className': 'numeric'}
+        ]
+    });
+    table.buttons().container().appendTo('#householdlist_wrapper .col-sm-6:eq(0)');
+    $('option[value=afar]').attr('selected', 'selected');
+});

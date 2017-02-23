0c4d33eb156b717ea8c5c295e2f79f38513b4eb3
mauroIstat <mbruno@istat.it>
2017-02-23 14:27:45 +0100

    Update mecbox.sql
    Rimosso indice

--- db/mecbox.sql
+++ db/mecbox.sql
@@ -43,9 +43,7 @@ CREATE TABLE `mb_user_roles` (
   `role` varchar(255) DEFAULT NULL,
   `userid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`user_role_id`),
-  KEY `FKqf4esriqbiqy7rmcxd2u67aml` (`userid`),
   CONSTRAINT `FK_USER_ROLES` FOREIGN KEY (`userid`) REFERENCES `mb_users` (`userid`),
-  CONSTRAINT `FKqf4esriqbiqy7rmcxd2u67aml` FOREIGN KEY (`userid`) REFERENCES `mb_users` (`userid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --

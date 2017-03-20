c147f0bebcbdfc052149833ce0bf977da75ec6aa
StefanoM <s.macone@libero.it>
2017-03-20 12:06:57 +0100

    Internaz

--- src/main/java/it/istat/mec/mecbox/MecboxApplication.java
+++ src/main/java/it/istat/mec/mecbox/MecboxApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  *
@@ -15,7 +17,13 @@ public class MecboxApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MecboxApplication.class, args);
     }
-
+    
+    @Bean
+    public ResourceBundleMessageSource messageSource () {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        return messageSource;
+    }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(MecboxApplication.class);
